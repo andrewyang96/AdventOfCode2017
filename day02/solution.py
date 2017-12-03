c6bd0def40138c63b9f1d2e1730b469131e758bd
Andrew Yang <ayang14@illinois.edu>
2017-12-02 20:14:26 -0600

    Forgot to complete day 2

--- day02/solution.py
+++ day02/solution.py
@@ -1,4 +1,15 @@
+import itertools
 from mypy.types import List
 
 def checksum(spreadsheet: List[List[int]]) -> int:
     return sum(map(lambda row: max(row) - min(row), spreadsheet))
+
+def checksum2(spreadsheet: List[List[int]]) -> int:
+    def process_row(row: List[int]) -> int:
+        for x, y in itertools.combinations(row, 2):
+            if x > y and x % y == 0:
+                return x // y
+            elif x < y and y % x == 0:
+                return y // x
+        raise RuntimeError()
+    return sum(map(process_row, spreadsheet))
--- day02/tests.py
+++ day02/tests.py
@@ -1,4 +1,5 @@
 from solution import checksum
+from solution import checksum2
 
 assert checksum([
     [5, 1, 9, 5],
@@ -9,3 +10,11 @@ assert checksum([
 with open('input.txt', 'r') as f:
     spreadsheet = [[int(num) for num in row.split()] for row in f]
 print(checksum(spreadsheet))
+
+assert checksum2([
+    [5, 9, 2, 8],
+    [9, 4, 7, 3],
+    [3, 8, 6, 5]
+]) == 9
+
+print(checksum2(spreadsheet))
