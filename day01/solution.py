6720c3b2a19dc5e3e7f694b0ffbcb2582bd8667d
Andrew Yang <ayang14@illinois.edu>
2017-12-02 20:07:23 -0600

    Forgot to complete day 1

--- day01/solution.py
+++ day01/solution.py
@@ -5,3 +5,11 @@ def captcha(num: str) -> int:
         if digit == next_digit:
             count += int(digit)
     return count
+
+def captcha2(num: str) -> int:
+    count = 0
+    for idx, digit in enumerate(num):
+        other_digit = num[idx+len(num)//2] if idx < len(num) / 2 else num[idx-len(num)//2]
+        if digit == other_digit:
+            count += int(digit)
+    return count
