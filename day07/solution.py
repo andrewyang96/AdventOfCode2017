fd65e939bf13a1d5570cd49032c747ce57e4c3d6
Andrew Yang <ayang14@illinois.edu>
2017-12-07 11:27:45 -0600

    Attempt day 7 part 2, gonna save for another time

--- day07/solution.py
+++ day07/solution.py
@@ -1,10 +1,17 @@
+from collections import Counter
+from collections import defaultdict
 from typing import Dict
+from typing import Tuple
 
 class Node(object):
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
-        self.children = set()
+        self.parent_name = None
+        self.children = set() # names of children, if any
+
+    def is_leaf(self):
+        return len(self.children) == 0
 
 def bottom_program(programs: Dict[str, Node]) -> str:
     potential_roots = set(programs.keys())
@@ -13,3 +20,39 @@ def bottom_program(programs: Dict[str, Node]) -> str:
     if len(potential_roots) != 1:
         raise ValueError('Must have exactly one program remaining: {0}'.format(potential_roots))
     return potential_roots.pop()
+
+def balance(programs: Dict[str, Node], bottom_name: str) -> Tuple[str, int]:
+    # returns the name of the corrected program and its corrected weight
+    weights = {}
+    def calculate_weight(name: str) -> int:
+        if name in weights:
+            return weights[name]
+        weights[name] = programs[name].weight + sum(
+            calculate_weight(child_name) for child_name in programs[name].children)
+        return weights[name]
+    calculate_weight(bottom_name)
+
+    queue = [bottom_name]
+    next_queue = []
+    while len(queue) > 0:
+        for name in queue:
+            node = programs[name]
+            if node.children:
+                child_weights = [weights[child_name] for child_name in node.children]
+                if len(set(child_weights)) == 2:
+                    imbalanced_weight = max(child_weights) if child_weights.count(max(child_weights)) == 1 else min(child_weights)
+                    target_weight = min(child_weights) if imbalanced_weight == max(child_weights) else max(child_weights)
+                    for child_name in node.children:
+                        print(weights[child_name], programs[child_name].weight)
+                        print(target_weight, imbalanced_weight, child_weights)
+                        if weights[child_name] == imbalanced_weight:
+                            print([programs[n].weight for n in programs[child_name].children])
+                            return (child_name,
+                                programs[child_name].weight + target_weight - imbalanced_weight)
+                elif len(set(child_weights)) >= 3:
+                    print('More than one imbalance found')
+            for child_name in node.children:
+                next_queue.append(child_name)
+        queue = next_queue
+        next_queue = []
+    raise ValueError('No imbalance found')
--- day07/tests.py
+++ day07/tests.py
@@ -1,17 +1,21 @@
 import re
 from solution import Node
 from solution import bottom_program
+from solution import balance
 
 pattern = re.compile(r'^(?P<name>\w+) \((?P<weight>\d+)\)( \-\> (?P<children>.*?))?$')
 
 def process_file(f):
     programs = {}
+    parents = {} # maps child to parent
     for line in f:
         match = pattern.match(line.strip())
         name, weight, children = match.group('name'), match.group('weight'), match.group('children')
         programs[name] = Node(name, int(weight))
         if children:
             programs[name].children = set(children.split(', '))
+    for child_name, parent_name in parents.items():
+        programs[child_name].parent_name = parent_name
     return programs
 
 with open('test_input.txt', 'r') as f:
@@ -21,4 +25,8 @@ with open('input.txt', 'r') as f:
     programs = process_file(f)
 
 assert bottom_program(test_programs) == 'tknk'
-print(bottom_program(programs))
+bottom_name = bottom_program(programs)
+print(bottom_name)
+
+assert balance(test_programs, 'tknk') == ('ugml', 60)
+print(balance(programs, bottom_name))
